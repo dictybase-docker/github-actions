4673eb41ec37885924b1821071c02a51f6a3b3f4
Siddhartha Basu <biosidd@gmail.com>
2019-08-27 09:13:48 -0500

    feat: add handler for issue tracker reporter

---
+++ internal/client/client.go
@@ -0,0 +1,14 @@
+package client
+
+import (
+	"github.com/google/go-github/v28/github"
+	"golang.org/x/oauth2"
+)
+
+func GetGithubClient(token string) (*github.Client, error) {
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	return github.NewClient(tc), nil
+}
---
+++ internal/command/issue/issue.go
@@ -0,0 +1,97 @@
+package issue
+
+import (
+	"context"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+
+	"github.com/google/go-github/v28/github"
+
+	"github.com/dictyBase-docker/github-actions/internal/client"
+	"github.com/urfave/cli"
+)
+
+const (
+	layout = "01/02/2006"
+)
+
+func CommentsReport(c *cli.Context) error {
+	var output io.Writer
+	if len(c.String("output")) > 0 {
+		wf, err := os.Create(c.String("output"))
+		if err != nil {
+			return cli.NewExitError(
+				fmt.Sprintf("unable to open file %s %s", c.String("output"), err),
+				2,
+			)
+		}
+		output = wf
+	} else {
+		output = os.Stdout
+	}
+	writer := csv.NewWriter(output)
+	gclient, err := client.GetGithubClient(c.GlobalString("token"))
+	if err != nil {
+		return cli.NewExitError(
+			fmt.Sprintf("error in getting github client %s", err),
+			2,
+		)
+	}
+	opt := &github.IssueListByRepoOptions{
+		State:       c.String("state"),
+		Sort:        "comments",
+		ListOptions: github.ListOptions{PerPage: 30},
+	}
+	writer.Write([]string{
+		"Issue ID", "Title", "Total Comments",
+		"Status", "Created On", "Closed On",
+	})
+	for {
+		issues, resp, err := gclient.Issues.ListByRepo(
+			context.Background(),
+			c.GlobalString("owner"),
+			c.GlobalString("repository"),
+			opt,
+		)
+		if err != nil {
+			return cli.NewExitError(
+				fmt.Sprintf("error in fetching issues %s", err),
+				2,
+			)
+		}
+		for _, iss := range issues {
+			if iss.IsPullRequest() {
+				continue
+			}
+			err := writer.Write([]string{
+				strconv.Itoa(iss.GetNumber()),
+				iss.GetTitle(),
+				strconv.Itoa(iss.GetComments()),
+				iss.GetState(),
+				iss.GetCreatedAt().Format(layout),
+				iss.GetClosedAt().Format(layout),
+			})
+			if err != nil {
+				return cli.NewExitError(
+					fmt.Sprintf("error in writing issues to csv file %s", err),
+					2,
+				)
+			}
+		}
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+	writer.Flush()
+	if err := writer.Error(); err != nil {
+		return cli.NewExitError(
+			fmt.Sprintf("error with csv writing %s", err),
+			2,
+		)
+	}
+	return nil
+}
