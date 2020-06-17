62f51b283408982666f391ff021458b7e9f2b493
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2020-06-17 17:15:46 -0500

    feat:add command line flags for setting deployment status

---
+++ internal/cmd/deploy.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"github.com/dictyBase-docker/github-actions/internal/app/deploy"
+	"github.com/urfave/cli"
+)
+
+func DeployStatusCmd() cli.Command {
+	return cli.Command{
+		Name:    "deploy-status",
+		Aliases: []string{"ds"},
+		Usage:   "create a github deployment status",
+		Action:  deploy.DeployStatus,
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "state",
+				Usage: "The state of the deployment status",
+			},
+			cli.StringFlag{
+				Name:  "deployment_id",
+				Usage: "Deployment identifier",
+			},
+			cli.StringFlag{
+				Name:  "url",
+				Usage: "The url that is associated with this status",
+			},
+		},
+	}
+}
