361dca6fb0d84e2b85dd8c1508a56dbb632c9e5e
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2020-06-18 07:36:22 -0500

    refactor:reduce no of error statements in SaveInS3 function

--- internal/app/storage/s3.go
+++ internal/app/storage/s3.go
@@ -2,7 +2,6 @@ package storage
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/dictyBase-docker/github-actions/internal/logger"
 	"github.com/minio/minio-go"
@@ -29,33 +28,16 @@ func SaveInS3(c *cli.Context) error {
 			2,
 		)
 	}
-	reader, err := os.Open(c.String("input"))
-	if err != nil {
-		return cli.NewExitError(
-			fmt.Sprintf("unable to read the file %s %s", c.String("input"), err),
-			2,
-		)
-	}
-	defer reader.Close()
-	fstat, err := reader.Stat()
-	if err != nil {
-		return cli.NewExitError(
-			fmt.Sprintf("unable to stat the file %s", err),
-			2,
-		)
-	}
 	l := logger.GetLogger(c)
-	l.Debugf("upload size of file %s is %d byte", c.String("input"), fstat.Size())
 	path := c.String("upload-path")
 	if len(path) == 0 {
 		path = c.String("input")
 	}
 	l.Debugf("upload path %s", path)
-	_, err = s3Client.PutObject(
+	_, err = s3Client.FPutObject(
 		c.String("s3-bucket"),
 		path,
-		reader,
-		fstat.Size(),
+		c.String("input"),
 		minio.PutObjectOptions{ContentType: "application/text"},
 	)
 	if err != nil {
