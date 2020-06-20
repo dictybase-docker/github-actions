d9c1ee8822eef99a9c3dc4fc4ba1ecfba82d4285
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2020-06-19 21:10:33 -0500

    refactor:replace oauth.NoContext with standard context call

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"context"
+
 	"github.com/google/go-github/v32/github"
 	"golang.org/x/oauth2"
 )
@@ -9,6 +11,6 @@ func GetGithubClient(token string) (*github.Client, error) {
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: token},
 	)
-	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	tc := oauth2.NewClient(context.Background(), ts)
 	return github.NewClient(tc), nil
 }
