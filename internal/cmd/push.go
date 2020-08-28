29bd35c6d35ec0e8b3bac6fffcf57a68e68e190e
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2020-08-28 12:56:27 -0500

    feat:add command line flags

---
+++ internal/cmd/push.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"github.com/dictyBase-docker/github-actions/internal/app/push"
+	"github.com/urfave/cli"
+)
+
+func PushFileCommited() cli.Command {
+	return cli.Command{
+		Name:    "push-file-committed",
+		Usage:   "outputs list of file committed in a git push",
+		Aliases: []string{"pfc"},
+		Action:  push.FileCommitted,
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:     "payload-file,f",
+				Required: true,
+				Usage:    "Full path to the file that contain the push payload",
+			},
+			cli.StringFlag{
+				Name:  "skip-file-suffix,sfs",
+				Usage: "file with the given suffix will not be reported",
+			},
+			cli.BoolTFlag{
+				Name:  "skip-deleted,sd",
+				Usage: "skip deleted files in the commit",
+			},
+			cli.StringFlag{
+				Name:  "output,o",
+				Usage: "Name of output file, defaults to stdout",
+			},
+		},
+	}
+}
