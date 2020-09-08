b2123db25b9bb543238f3da67aa55ed7894f5bae
wildlifehexagon <crossthebreeze@gmail.com>
2020-09-08 14:48:32 -0500

    refactor: add real logic to pr deploy command

--- internal/app/chatops/pr.go
+++ internal/app/chatops/pr.go
@@ -1,17 +1,35 @@
 package chatops
 
 import (
+	"fmt"
+
 	"github.com/dictyBase-docker/github-actions/internal/logger"
 	"github.com/sethvargo/go-githubactions"
 	"github.com/urfave/cli"
 )
 
 func PullRequestDeploy(c *cli.Context) error {
+	cluster := c.String("cluster")
+	commit := c.String("commit")
+	sha := c.String("sha")
+	prid := c.String("pr-id")
+	var ref, imageTag string
+
 	a := githubactions.New()
 	log := logger.GetLogger(c)
-	a.SetOutput("cluster", c.String("cluster"))
-	a.SetOutput("ref", "")
-	a.SetOutput("image_tag", "")
+
+	if commit != "" {
+		ref = commit
+		imageTag = fmt.Sprintf("pr-%s-%s", prid, commit[0:6])
+	} else {
+		ref = sha
+		imageTag = fmt.Sprintf("pr-%s-%s", prid, sha[0:6])
+	}
+
+	a.SetOutput("cluster", cluster)
+	a.SetOutput("ref", ref)
+	a.SetOutput("image_tag", imageTag)
 	log.Info("added all keys to the output")
+
 	return nil
 }
