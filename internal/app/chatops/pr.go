a30950c3e9c57645c6377e9a6fcc64deb828e85c
wildlifehexagon <crossthebreeze@gmail.com>
2020-09-08 15:52:21 -0500

    refactor: add function for converting cli input to desired output

--- internal/app/chatops/pr.go
+++ internal/app/chatops/pr.go
@@ -8,28 +8,50 @@ import (
 	"github.com/urfave/cli"
 )
 
-func PullRequestDeploy(c *cli.Context) error {
-	cluster := c.String("cluster")
-	commit := c.String("commit")
-	sha := c.String("sha")
-	prid := c.String("pr-id")
-	var ref, imageTag string
+type Input struct {
+	Commit        string
+	SHA           string
+	PullRequestID string
+}
 
+type Output struct {
+	ImageTag string
+	Ref      string
+}
+
+func PullRequestDeploy(c *cli.Context) error {
+	i := &Input{
+		Commit:        c.String("commit"),
+		SHA:           c.String("sha"),
+		PullRequestID: c.String("pr-id"),
+	}
 	a := githubactions.New()
 	log := logger.GetLogger(c)
 
-	if commit != "" {
-		ref = commit
-		imageTag = fmt.Sprintf("pr-%s-%s", prid, commit[0:6])
-	} else {
-		ref = sha
-		imageTag = fmt.Sprintf("pr-%s-%s", prid, sha[0:6])
-	}
+	o := convertToOutput(i)
 
-	a.SetOutput("cluster", cluster)
-	a.SetOutput("ref", ref)
-	a.SetOutput("image_tag", imageTag)
+	a.SetOutput("cluster", c.String("cluster"))
+	a.SetOutput("ref", o.Ref)
+	a.SetOutput("image_tag", o.ImageTag)
 	log.Info("added all keys to the output")
 
 	return nil
+}
+
+func convertToOutput(i *Input) *Output {
+	commit := i.Commit
+	sha := i.SHA
+	prid := i.PullRequestID
+	var ref, imageTag string
+	if commit != "" {
+		ref = commit
+		imageTag = fmt.Sprintf("pr-%s-%s", prid, commit[0:7])
+	} else {
+		ref = sha
+		imageTag = fmt.Sprintf("pr-%s-%s", prid, sha[0:7])
+	}
+	return &Output{
+		ImageTag: imageTag,
+		Ref:      ref,
+	}
 }
