c09c4675b3536bebb007b9614dc2f7bb93987254
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2020-09-19 04:39:36 -0500

    refactor:create new file package for InputOutput method

--- internal/app/push/push.go
+++ internal/app/push/push.go
@@ -2,24 +2,22 @@ package push
 
 import (
 	"fmt"
-	"io"
-	"os"
 	"strings"
 
-	"github.com/dictyBase-docker/github-actions/internal/client"
+	"github.com/dictyBase-docker/github-actions/internal/file"
 	gh "github.com/dictyBase-docker/github-actions/internal/github"
 	"github.com/dictyBase-docker/github-actions/internal/logger"
 	"github.com/urfave/cli"
 )
 
 func PushFileCommited(c *cli.Context) error {
-	in, out, err := inputOutput(c)
+	in, out, err := file.InputOutput(c)
 	if err != nil {
 		return cli.NewExitError(err.Error(), 2)
 	}
 	defer in.Close()
 	defer out.Close()
-	files, err := changedFiles(c, in)
+	files, err := gh.FilterCommittedFiles(c, in, "push")
 	if err != nil {
 		return cli.NewExitError(err.Error(), 2)
 	}
@@ -31,39 +29,4 @@ func PushFileCommited(c *cli.Context) error {
 		log.Infof("%d files has changed in the push", len(files))
 	}
 	return nil
-}
-
-func inputOutput(c *cli.Context) (*os.File, *os.File, error) {
-	var in *os.File
-	var out *os.File
-	r, err := os.Open(c.String("payload-file"))
-	if err != nil {
-		return in, out, fmt.Errorf("error in reading content from file %s", err)
-	}
-	in = r
-	if len(c.String("output")) > 0 {
-		w, err := os.Create(c.String("output"))
-		if err != nil {
-			return in, out, fmt.Errorf("error in creating file %s %s", c.String("output"), err)
-		}
-		out = w
-	} else {
-		out = os.Stdout
-	}
-	return in, out, nil
-}
-
-func changedFiles(c *cli.Context, in io.Reader) ([]string, error) {
-	gclient, err := client.GetGithubClient(c.GlobalString("token"))
-	if err != nil {
-		return []string{}, fmt.Errorf("error in getting github client %s", err)
-	}
-	fb, err := gh.NewGithubManager(gclient).CommittedFilesInPush(in)
-	if err != nil {
-		return []string{}, err
-	}
-	return fb.FilterUniqueByName().
-		FilterDeleted(c.BoolT("skip-deleted")).
-		FilterSuffix(c.String("include-file-suffix")).
-		List(), nil
 }
