0af5b490f622dbc0cb0b323ccaa4db77a33ec918
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2020-09-19 06:28:53 -0500

    feat:add FilesCommited function to handle both push and pull

--- internal/app/repository/commit.go
+++ internal/app/repository/commit.go
@@ -1,23 +1,37 @@
-package push
+package repository
 
 import (
 	"fmt"
 	"strings"
 
+	"github.com/dictyBase-docker/github-actions/internal/client"
 	"github.com/dictyBase-docker/github-actions/internal/file"
 	gh "github.com/dictyBase-docker/github-actions/internal/github"
 	"github.com/dictyBase-docker/github-actions/internal/logger"
 	"github.com/urfave/cli"
 )
 
-func PushFileCommited(c *cli.Context) error {
+func FilesCommited(c *cli.Context) error {
+	gclient, err := client.GetGithubClient(c.GlobalString("token"))
+	if err != nil {
+		return cli.NewExitError(
+			fmt.Sprintf("error in getting github client %s", err),
+			2,
+		)
+	}
 	in, out, err := file.InputOutput(c)
 	if err != nil {
 		return cli.NewExitError(err.Error(), 2)
 	}
 	defer in.Close()
 	defer out.Close()
-	files, err := gh.FilterCommittedFiles(c, in, "push")
+	files, err := gh.FilterCommittedFiles(&gh.CommittedFilesParams{
+		Client:      gclient,
+		Input:       in,
+		Event:       c.String("event-type"),
+		FileSuffix:  c.String("include-file-suffix"),
+		SkipDeleted: c.BoolT("skip-deleted"),
+	})
 	if err != nil {
 		return cli.NewExitError(err.Error(), 2)
 	}
